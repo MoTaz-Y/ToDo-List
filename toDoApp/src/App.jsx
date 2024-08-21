f88bf60153fa447bae3a0fb623d77cd24b28f23d
motaz_y <motazyasser84@gmail.com>
2024-08-21 20:31:57 +0300

    home page setup

--- toDoApp/src/App.jsx
+++ toDoApp/src/App.jsx
@@ -1,9 +1,16 @@
+import "./App.css";
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <header className="App-header">
         <h1>To Do App</h1>
       </header>
+
+      <main className="app-main">
+        <section className="task-column">Section 1</section>
+        <section className="task-column">Section 2</section>
+        <section className="task-column">Section 3</section>
+      </main>
     </div>
   );
 };
