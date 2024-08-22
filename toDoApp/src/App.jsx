047e8374723f42dcaaaab4a8e543e925c1530dad
motaz_y <motazyasser84@gmail.com>
2024-08-22 18:01:43 +0300

    local storage handele

--- toDoApp/src/App.jsx
+++ toDoApp/src/App.jsx
@@ -4,9 +4,16 @@ import TaskForm from "./assets/components/TaskForm";
 import ToDo from "./assets/images/cscsc.png";
 import InProgress from "./assets/images/42656-glowing-star-icon.png";
 import Completed from "./assets/images/pngtree-3d-green-check-icon-in-transparent-background-png-image_6552254.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const oldTasks = localStorage.getItem("tasks");
+console.log(oldTasks);
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks) || []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const handleDelete = (index) => {
     const newTasks = tasks.filter((task, i) => i !== index);
